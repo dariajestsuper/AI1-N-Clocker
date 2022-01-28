381fdce2fb8c289828cf9469fc063fc8561dff04
mateusz.malecki <mateusz.malecki@dotfusion.eu>
2022-01-28 20:57:42 +0100

    applied previous server setup

--- server/public/index.php
+++ server/public/index.php
@@ -1,9 +1,14 @@
 <?php
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
 
-use App\Kernel;
+// Autoloader
+require_once '../vendor/autoload.php';
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+// Load Config
+require_once '../config/config.php';
 
-return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
-};
+// Routes
+require_once '../routes/web.php';
+require_once '../src/Router.php';
---
+++ server/views/layout/base.php
@@ -0,0 +1,6 @@
+<?php
+function insertBody(string $path)
+{
+    require_once APP_ROOT . $path;
+}
+require_once APP_ROOT . '/views/layout/header.php';
